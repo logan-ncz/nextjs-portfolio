782171e2cb41f21e9d58e1de3a44640b122c092f
Logan Nochez <logannochez@hotmail.fr>
2023-10-29 16:25:54 +0100

    First commit

---
+++ data/projects.js
@@ -0,0 +1,62 @@
+export const projects = [
+	{
+		id: "1",
+		title: "React Redux Todolist",
+		thumbnail: "react_redux_todolist",
+		githubpages: "https://logan-ncz.github.io/react-redux-todolist/",
+	},
+	{
+		id: "2",
+		title: "Kasa",
+		thumbnail: "kasa",
+		githubpages: "https://logan-ncz.github.io/kasa/",
+	},
+	{
+		id: "3",
+		title: "HRnet",
+		thumbnail: "hrnet",
+		githubpages: "https://logan-ncz.github.io/hrnet/",
+	},
+	{
+		id: "4",
+		title: "Argent Bank",
+		thumbnail: "argentbank",
+		githubpages: "https://logan-ncz.github.io/argentbank/",
+	},
+	{
+		id: "5",
+		title: "SportSee",
+		thumbnail: "sportsee",
+		githubpages: "https://logan-ncz.github.io/sportsee/",
+	},
+	{
+		id: "6",
+		title: "Bill App",
+		thumbnail: "billed",
+		githubpages: "https://github.com/logan-ncz/bill_app",
+	},
+	{
+		id: "7",
+		title: "Les Petits Plats",
+		thumbnail: "les_petits_plats",
+		githubpages: "https://logan-ncz.github.io/les_petits_plats/",
+	},
+	{
+		id: "8",
+		title: "FishEye",
+		thumbnail: "fisheye",
+		githubpages: "https://logan-ncz.github.io/fisheye/",
+	},
+	{
+		id: "9",
+		title: "GameOn",
+		thumbnail: "gameon",
+		githubpages: "https://logan-ncz.github.io/gameon/",
+	},
+	{
+		id: "10",
+		title: "Ohmyfood",
+		thumbnail: "ohmyfood",
+		githubpages: "https://logan-ncz.github.io/ohmyfood/",
+	},
+];
