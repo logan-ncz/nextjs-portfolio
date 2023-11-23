3fa5e9626ffab1b4b7d9d96fc7793dc136c978da
Logan Nochez <logannochez@hotmail.fr>
2023-11-24 00:36:55 +0100

    rename icons logoStack, add project's informations

--- data/projects.js
+++ data/projects.js
@@ -3,70 +3,80 @@ export const projects = [
 		id: "1",
 		title: "React Redux Todolist",
 		thumbnail: "react_redux_todolist",
+		githubRepository: "https://github.com/logan-ncz/react-redux-todolist",
 		githubpages: "https://logan-ncz.github.io/react-redux-todolist/",
-		stack: ["react-2", "redux"]
+		stack: ["react", "redux", "sass"]
 	},
 	{
 		id: "2",
 		title: "Kasa",
 		thumbnail: "kasa",
+		githubRepository: "https://github.com/logan-ncz/kasa",
 		githubpages: "https://logan-ncz.github.io/kasa/",
-		stack: ["react-2", "redux"]
+		stack: ["react"]
 	},
 	{
 		id: "3",
 		title: "HRnet",
 		thumbnail: "hrnet",
+		githubRepository: "https://github.com/logan-ncz/hrnet",
 		githubpages: "https://logan-ncz.github.io/hrnet/",
-		stack: ["react-2", "redux"]
+		stack: ["javascript", "jest"]
 	},
 	{
 		id: "4",
 		title: "Argent Bank",
 		thumbnail: "argentbank",
+		githubRepository: "https://github.com/logan-ncz/argentbank",
 		githubpages: "https://logan-ncz.github.io/argentbank/",
-		stack: ["react-2", "redux"]
+		stack: ["react", "redux"]
 	},
 	{
 		id: "5",
 		title: "SportSee",
 		thumbnail: "sportsee",
+		githubRepository: "https://github.com/logan-ncz/sportsee",
 		githubpages: "https://logan-ncz.github.io/sportsee/",
-		stack: ["react-2", "redux"]
+		stack: ["react"]
 	},
 	{
 		id: "6",
 		title: "Bill App",
 		thumbnail: "billed",
+		githubRepository: "https://github.com/logan-ncz/bill_app",
 		githubpages: "https://github.com/logan-ncz/bill_app",
-		stack: ["react-2", "redux"]
+		stack: ["javascript"]
 	},
 	{
 		id: "7",
 		title: "Les Petits Plats",
 		thumbnail: "les_petits_plats",
+		githubRepository: "https://github.com/logan-ncz/les_petits_plats",
 		githubpages: "https://logan-ncz.github.io/les_petits_plats/",
-		stack: ["react-2", "redux"]
+		stack: ["html-5", "sass", "javascript"]
 	},
 	{
 		id: "8",
 		title: "FishEye",
 		thumbnail: "fisheye",
+		githubRepository: "https://github.com/logan-ncz/fisheye",
 		githubpages: "https://logan-ncz.github.io/fisheye/",
-		stack: ["react-2", "redux"]
+		stack: ["html-5", "sass", "javascript"]
 	},
 	{
 		id: "9",
 		title: "GameOn",
 		thumbnail: "gameon",
+		githubRepository: "https://github.com/logan-ncz/gameon",
 		githubpages: "https://logan-ncz.github.io/gameon/",
-		stack: ["react-2", "redux"]
+		stack: ["javascript"]
 	},
 	{
 		id: "10",
 		title: "Ohmyfood",
 		thumbnail: "ohmyfood",
+		githubRepository: "https://github.com/logan-ncz/ohmyfood",
 		githubpages: "https://logan-ncz.github.io/ohmyfood/",
-		stack: ["react-2", "redux"]
+		stack: ["html-5", "sass"]
 	},
 ];
